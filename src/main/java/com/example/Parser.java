6244bfb237520c03c7b7a6e65aa001dd4ee2f238
Artem Doronin <doronintema2018@yandex.ru>
2026-06-03 01:00:55 +0500

    parser update

--- src/main/java/com/example/Parser.java
+++ src/main/java/com/example/Parser.java
@@ -1,7 +1,40 @@
 package com.example;
 
+import java.util.Arrays;
+
 public class Parser {
     public Command parse(String input) {
-        return null;
+        String[] array = input.split(" ");
+        String strCommand = input.split(" ")[0];
+        String strId = input.split(" ")[1];
+        Long id;
+        String value;
+        boolean availabilityOfIdInRequest;
+        if (strCommand.equals("CREATE") || (strCommand.equals("GET") && (!isLong(strId)))) {
+            id = null;
+            availabilityOfIdInRequest = false;
+            String[] result = Arrays.copyOfRange(array, 1, array.length);
+            value = String.join(" ", result);
+        } else {
+            id = Long.parseLong(strId);
+            availabilityOfIdInRequest = true;
+            String[] result = Arrays.copyOfRange(array, 2, array.length);
+            value = String.join(" ", result);
+        }
+
+        return new Command(id, availabilityOfIdInRequest, strCommand, value);
     }
+
+    private static boolean isLong(String str) {
+        if (str == null) {
+            return false;
+        }
+        try {
+            Long.parseLong(str);
+            return true;
+        } catch (NumberFormatException e) {
+            return false;
+        }
+    }
+
 }
