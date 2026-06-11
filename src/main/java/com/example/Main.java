72b7c4a540d4fc00f67cc9c82c8247582181bd51
Artem Doronin <doronintema2018@yandex.ru>
2026-06-11 15:11:00 +0500

    test

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -6,7 +6,6 @@ import com.example.test.ValidatorTest;
 
 public class Main {
 
-
     public static void main(String[] args) {
 
         System.out.println("\n========================================");
